9dbbdfb861d0569ae076595c39bb03119b859628
Mhst06 <1186217640@qq.com>
2021-12-09 00:02:57 +0800

    1.接入redis

--- miaosha001/src/main/java/com/example/miaosha/miaosha001/redis/RedisConfig.java
+++ miaosha001/src/main/java/com/example/miaosha/miaosha001/redis/RedisConfig.java
@@ -1,4 +1,30 @@
 package com.example.miaosha.miaosha001.redis;
 
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
+
+
+@EnableCaching
+@Configuration
 public class RedisConfig {
+
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
 }
